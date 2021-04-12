ba0b87cfc4fdfda0a855b492ddaffcdd9579175d
Eckhart Köppen <eck@40hz.org>
2021-04-12 10:23:17 +0300

    Build based on GNAT CE 2020

--- bb-runtimes.sh
+++ bb-runtimes.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-if [ ${TARGET} != "arm-eabi" ]; then
+if [ "${TARGET}" != "arm-eabi" ]; then
     echo "Skipping build for non-arm target ${TARGET}"
+    exit
 fi
 
 rm -rf install
@@ -10,19 +11,9 @@ prefix=$1; shift
 gcc_dir=$1; shift
 gnat_dir=$1; shift
 
-./gen_rts_sources.py --gcc-dir "${gcc_dir}" --gnat-dir "${gnat_dir}" --rts-profile ravenscar-sfp
+./gen_rts_sources.py --gcc-dir "${gcc_dir}" --gnat-dir "${gnat_dir}" --rts-profile ravenscar-full
 
 for target in $@
 do
-    ./build_rts.py  --rts-src-descriptor ./install/lib/gnat/rts-sources.json "${target}"
-    for rts in zfp ravenscar-sfp
-    do
-        if [ ! -d install/"${rts}-${target}" ]; then continue; fi
-        cd install/"${rts}-${target}"
-        gprbuild -q -p -P runtime_build.gpr
-        mkdir -p "${prefix}/rts-${rts}-${target}"
-        cp -R adalib "${prefix}/rts-${rts}-${target}/"
-        cp -R gnat "${prefix}/rts-${rts}-${target}/adainclude"
-        cd ../..
-    done
+    ./build_rts.py --output ${prefix} --build --rts-src-descriptor ./install/lib/gnat/rts-sources.json "${target}"
 done
